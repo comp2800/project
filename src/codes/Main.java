ee2c3f03e2402c54791542c3c205f717b4825c96
Yanx <pyama.ndiaye@gmail.com>
2023-04-06 14:08:58 -0400

    Menu reformat

--- src/codes/Main.java
+++ src/codes/Main.java
@@ -9,27 +9,18 @@ import static codes.Game.create_Scene;
 
 public class Main implements ActionListener {
 
-    private GameState gameState;
-    private boolean running = true;
-    private boolean restartLevel = false;
-
     Main() throws IOException {
         JFrame f = new JFrame("Flight Simulator");
         f.setSize(1920, 1080);
 
-        gameState = new GameState();
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JMenuBar jmb = new JMenuBar();
 
         JMenu jmFile = new JMenu("File");
         JMenuItem jmiNewGame = new JMenuItem("New Game");
-        JMenuItem jmiRestartLevel = new JMenuItem("Restart Level");
-        JMenuItem jmiExit = new JMenuItem("Exit");
         jmFile.add(jmiNewGame);
-        jmFile.add(jmiRestartLevel);
         jmFile.addSeparator();
-        jmFile.add(jmiExit);
         jmb.add(jmFile);
 
         f.getContentPane().add(new Game(create_Scene()));
@@ -43,25 +34,6 @@ public class Main implements ActionListener {
             }
         });
 
-        jmiRestartLevel.addActionListener((ActionEvent e) -> {
-            System.out.println("Restarting level " + gameState.getCurrentLevel());
-            restartLevel = true; // set flag to indicate level should be restarted
-
-            while (running) {
-                if (restartLevel) {
-                    // reload the current level
-                    gameState.resetLevel(); // reset game state for current level
-                    System.out.println("Reloading level " + gameState.getCurrentLevel());
-                    restartLevel = false; // clear the flag
-                }
-                // update game logic and render graphics
-            }
-        });
-
-        jmiExit.addActionListener((ActionEvent e) -> {
-            System.exit(0);
-        });
-
         f.setJMenuBar(jmb);
         f.setVisible(true);
     }
@@ -71,29 +43,5 @@ public class Main implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent ae) {
-        String comStr = ae.getActionCommand();
-        System.out.println(comStr + " Selected");
-    }
-
-    private class GameState {
-        private int currentLevel;
-        private int score;
-
-        public GameState() {
-            currentLevel = 1;
-            score = 0;
-        }
-
-        public int getCurrentLevel() {
-            return currentLevel;
-        }
-
-        public void resetLevel() {
-            // Reset game state for current level
-            score = 0;
-            // ...
-        }
-
-        // Other methods for updating game state such as updating score, etc.
     }
 }
--- src/codes/TurretBehaviour.java
+++ src/codes/TurretBehaviour.java
@@ -2,6 +2,7 @@ package codes;
 
 import org.jogamp.java3d.*;
 import org.jogamp.java3d.utils.geometry.Box;
+import org.jogamp.vecmath.Point3d;
 
 import static codes.Commons.hundredBS;
 
@@ -13,7 +14,9 @@ public class TurretBehaviour {
     public TurretBehaviour() {
         ammoBG = new BranchGroup();
         Appearance app = Commons.obj_Appearance(Commons.Green);
-        Box laser = new Box(0.09f, 0.03f, 0.09f, app);
+        Box laser = new Box(0.1f, 0.1f, 0.1f, app);
+        laser.setCapability(Shape3D.ALLOW_BOUNDS_WRITE);
+        laser.setBounds(new BoundingSphere(new Point3d(0, 0, 0), 1));
         TransformGroup turretTG = new TransformGroup();
         turretTG.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
         turretTG.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
@@ -21,7 +24,7 @@ public class TurretBehaviour {
         ammoBG.addChild(turretTG);
         Transform3D axis = new Transform3D();
         axis.rotY(Math.PI / 2);
-        PositionInterpolator getter = new PositionInterpolator(alpha, turretTG, axis, 0.0f, 100.0f);
+        PositionInterpolator getter = new PositionInterpolator(alpha, turretTG, axis, 0.0f, 50.0f);
         getter.setSchedulingBounds(hundredBS);
         ammoBG.addChild(getter);
         aimBot.addChild(ammoBG);
